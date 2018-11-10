b8019d96f8f882ef91d07b66352079c19c621159
blooblahguy <blooblahguy@gmail.com>
2018-11-10 14:23:07 -0700

    tooltips remake, docs update, modal css

--- demo/elements.php
+++ demo/elements.php
@@ -7,7 +7,7 @@
 <div class="section">
 	<div class="container">
 		<p>If you are looking for a library that gives you all of the UI elements you could ever possible want, this is probably not the framework for you.</p>
-		<p>There are a few important UI elements that are important for the user experience. Here's our take on those elements.</p>
+		<p>There are few UI elements that are important for the user experience. Here's our take on those elements.</p>
 		<h3>Buttons</h3>
 		<p>Buttons are diverse, sometimes you want them to display inline, other times as fat blocks. For that reason we try and leave the default buttons similar to how the browser intended. You can still make buttons full with with <kbd>.block</kbd> or add padding with the <kbd>.pad[n]</kbd> helpers outlines in Layouts.</p>
 		<div class="row g2">
@@ -15,6 +15,7 @@
 			<div class="os-md-4 os-6"><a href="#0" class="btn-primary">Button Primary</a></div>
 			<div class="os-md-4 os-6"><a href="#0" class="btn-secondary">Button Secondary</a></div>
 			<div class="os-md-4 os-6"><a href="#0" class="btn-white">Button White</a></div>
+			<div class="os-md-4 os-6"><a href="#0" class="btn-blank">Button Blank</a></div>
 			<div class="os-md-4 os-6"><a href="#0" class="btn-grey">Button Grey</a></div>
 			<div class="os-md-4 os-6"><a href="#0" class="btn-dark">Button Dark</a></div>
 			<div class="os-md-4 os-6"><a href="#0" class="btn-black">Button Black</a></div>
@@ -24,7 +25,54 @@
 			<div class="os-md-4 os-6"><a href="#0" class="btn-error">Button Error</a></div>
 		</div>
 		<h3>Tooltips</h3>
+		<p>Purely HTML tooltips are good way to show additional information on hover or activate states for users. <kbd>[tooltip=""]</kbd> will show a default tooltip on top of the element.</p>
+		<ul>
+			<li><kbd>[tooltip="tooltip example"]</kbd> Populates the tooltip text and display it on top of <div class="btn btn-sm" tooltip="tooltip example">the object</div>.</li>
+			<li>Adding<kbd>[tooltip-right]</kbd> Displays the tooltip on the right of <div class="btn btn-sm" tooltip="tooltip example" tooltip-right>the object</div>.</li>
+			<li>Adding<kbd>[tooltip-bottom]</kbd> Displays the tooltip on the bottom of <div class="btn btn-sm" tooltip="tooltip example" tooltip-bottom>the object</div>.</li>
+			<li>Adding<kbd>[tooltip-left]</kbd> Displays the tooltip on the left of <div class="btn btn-sm" tooltip="tooltip example" tooltip-left>the object</div>.</li>
+		</ul>
 		<h3>Modals</h3>
+		<p>Modals are one of the only parts of this library that are not pure CSS. They can be made to be pure CSS, but you would need to have all of it's content inside of the HTML and hidden and you limit the ability for ajax-loaded content. We do a hybrid installment instead. You can still do pure css / html modals, but in order to expand their functionality you'll need openskull.js as well.</p>
+		<ul>
+			<li><mark class="bg-success em">Pure CSS</mark>: Any element who's href links to the <kbd>[id]</kbd> of a modal window will display the modal window.</li>
+			<li><mark class="bg-warning em">Javascript</mark>:  Elements marked with the <kbd>[modal]</kbd> attribute will be picked up by the javascript and create/populate the modal window with the <kbd>[href]</kbd> target.</li>
+		</ul>
+		<div class="modal_css">
+			<a href="#example_modal" class="btn">Pure CSS Modal Link</a>
+			<div class="modal" id="example_modal">
+				<a class="modal_bg" href="#0"></a>
+				<div class="modal_body">
+					<h2>Example Modal Window</h2>
+					This is the content inside of this modal window. You'll notice that the hash of the window url has changed to this target.
+					<form action="#0">
+						<em>This could be a good place for a form</em>
+						<div class="row g1">
+							<div class="os">
+								<label for="">First Name</label>
+								<input type="text" placeholder="First Name">
+							</div>
+							<div class="os">
+								<label for="">Last Name</label>
+								<input type="text" placeholder="Last Name">
+							</div>
+							<div class="clear"></div>
+							<div class="os">
+								<label for="">Email <em>*</em></label>
+								<input type="email" required placeholder="Email*">
+							</div>
+						</div>
+					</form>
+					<div class="row buttons padt2">
+						<div class="os">
+							<a href="#0" class="btn-primary">Accept Modal</a>
+						</div>
+						<div class="os text-right"><a href="#0" class="btn-blank text-error">Close Modal</a></div>
+					</div>
+					<a class="modal_close" href="#0"></a>
+				</div>
+			</div>
+		</div>
 		<h3>Tabs</h3>
 		<div class="tabs">
 			<div class="tab_nav">
