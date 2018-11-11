7b08ec97123390e4c3797eecc1856a6ebeef23a4
blooblahguy <blooblahguy@gmail.com>
2018-11-10 18:48:44 -0700

    tooltip reset, type fixes, demo nearly finished

--- demo/elements.php
+++ demo/elements.php
@@ -10,19 +10,21 @@
 		<p>There are few UI elements that are important for the user experience. Here's our take on those elements.</p>
 		<h3>Buttons</h3>
 		<p>Buttons are diverse, sometimes you want them to display inline, other times as fat blocks. For that reason we try and leave the default buttons similar to how the browser intended. You can still make buttons full with with <kbd>.block</kbd> or add padding with the <kbd>.pad[n]</kbd> helpers outlines in Layouts.</p>
-		<div class="row g2">
-			<div class="os-md-4 os-6"><a href="#0" class="btn">Button Default</a></div>
-			<div class="os-md-4 os-6"><a href="#0" class="btn-primary">Button Primary</a></div>
-			<div class="os-md-4 os-6"><a href="#0" class="btn-secondary">Button Secondary</a></div>
-			<div class="os-md-4 os-6"><a href="#0" class="btn-white">Button White</a></div>
-			<div class="os-md-4 os-6"><a href="#0" class="btn-blank">Button Blank</a></div>
-			<div class="os-md-4 os-6"><a href="#0" class="btn-grey">Button Grey</a></div>
-			<div class="os-md-4 os-6"><a href="#0" class="btn-dark">Button Dark</a></div>
-			<div class="os-md-4 os-6"><a href="#0" class="btn-black">Button Black</a></div>
-			<div class="os-md-4 os-6"><a href="#0" class="btn-success">Button Success</a></div>
-			<div class="os-md-4 os-6"><a href="#0" class="btn-info">Button Info</a></div>
-			<div class="os-md-4 os-6"><a href="#0" class="btn-warning">Button Warning</a></div>
-			<div class="os-md-4 os-6"><a href="#0" class="btn-error">Button Error</a></div>
+		<div class="row g2 text-center">
+			<div class="os-md-4 os-6"><a href="#0" class="btn">.btn</a></div>
+			<div class="os-md-4 os-6"><a href="#0" class="btn-primary">.btn-primary</a></div>
+			<div class="os-md-4 os-6"><a href="#0" class="btn-secondary">.btn-secondary</a></div>
+			<div class="os-md-4 os-6"><a href="#0" class="btn-white">.btn-white</a></div>
+			<div class="os-md-4 os-6"><a href="#0" class="btn-blank">.btn-blank</a></div>
+			<div class="os-md-4 os-6"><a href="#0" class="btn-grey">.btn-grey</a></div>
+			<div class="os-md-4 os-6"><a href="#0" class="btn-dark">.btn-dark</a></div>
+			<div class="os-md-4 os-6"><a href="#0" class="btn-black">.btn-black</a></div>
+			<div class="os-md-4 os-6"><a href="#0" class="btn-success">.btn-success</a></div>
+			<div class="os-md-4 os-6"><a href="#0" class="btn-info">.btn-info</a></div>
+			<div class="os-md-4 os-6"><a href="#0" class="btn-warning">.btn-warning</a></div>
+			<div class="os-md-4 os-6"><a href="#0" class="btn-error">.btn-erro</a></div>
+			<div class="os-md-4 os-6"><a href="#0" class="btn-primary btn-lg">.btn-primary.btn-lg</a></div>
+			<div class="os-md-4 os-6"><a href="#0" class="btn-primary btn-sm">.btn-primary.btn-sm</a></div>
 		</div>
 		<h3>Tooltips</h3>
 		<p>Purely HTML tooltips are good way to show additional information on hover or activate states for users. <kbd>[tooltip=""]</kbd> will show a default tooltip on top of the element.</p>
@@ -43,7 +45,7 @@
 			<div class="modal" id="example_modal">
 				<a class="modal_bg" href="#0"></a>
 				<div class="modal_body">
-					<h2>Example Modal Window</h2>
+					<h1>Example Modal Window</h1>
 					This is the content inside of this modal window. You'll notice that the hash of the window url has changed to this target.
 					<form action="#0">
 						<em>This could be a good place for a form</em>
@@ -76,56 +78,37 @@
 		<h3>Tabs</h3>
 		<div class="tabs">
 			<div class="tab_nav">
-				<a tab="tabs">Tabs</a>
-				<a tab="buttons">Buttons</a>
-				<a tab="pagination">Pagination</a>
-				<a tab="tooltips">Tooltips</a>
-				<a tab="modal">Modal</a>
+				<a tab="tab1">Tab 1</a>
+				<a tab="tab2">Tab 2</a>
+				<a tab="tab3">Tab 3</a>
 			</div>
-			<div class="tab_content" tab="tabs">
-				<h4>Tabs</h4>
-				here we are
-			</div>
-
-			<div class="tab_content" tab="buttons">
-				<h4>Buttons</h4>
-				<a href="#0" class="btn block btn-primary">.btn.block.btn-primary</a>
-
-				<!-- <div class="row content-space padt20"> -->
-					<a href="#0" class="btn btn-lg text-center">.text-center > .btn.btn-lg</a>
-					<a href="#0" class="btn btn-sm pull-left">.btn.btn-sm.pull-left</a>
-					<a href="#0" class="btn btn-xs pull-right">.btn.btn-xs.pull-right</a>
-
-				<!-- </div> -->
-				<!-- <div class="row content-space padt20"> -->
-					<a href="#0" class="btn btn-primary">.btn-primary</a>
-					<a href="#0" class="btn btn-info">.btn-info</a>
-					<a href="#0" class="btn btn-warning">.btn-warning</a>
-					<a href="#0" class="btn btn-danger">.btn-danger</a>
-				<!-- </div> -->
+			<div class="tab_content" tab="tab1">
+				<h4>Tab 1</h4>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sagittis urna mi, nec scelerisque lacus tempor in. Sed congue lacus quis diam condimentum malesuada. Duis finibus volutpat neque, a tincidunt dui sagittis eget. Aenean in purus dignissim dui gravida vulputate. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesent consectetur sit amet arcu vitae efficitur. Nullam varius mi at tellus scelerisque efficitur. Nam in dui dui. Cras maximus urna vitae dolor auctor tincidunt. Aliquam in semper sem. Pellentesque dignissim venenatis ullamcorper. Integer ac dolor consectetur, aliquet nulla a, rhoncus arcu.</p>
+				<p>Fusce quis posuere est, sed tempus nisl. Duis finibus ullamcorper aliquam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Maecenas imperdiet lobortis magna vel volutpat. Cras augue dui, tempor eget eleifend eget, tempor vitae enim. Mauris nulla sapien, viverra quis nisi eget, tempus ultricies libero. Donec non tristique nisi, sed accumsan mi. Etiam quis nisi metus. Proin volutpat eu odio maximus efficitur. Quisque sagittis rutrum velit, sit amet accumsan quam consectetur ac. Donec non nisl vel tellus lacinia pellentesque vel ac sapien. Nam iaculis porta posuere. i</p>
 			</div>
 
-			<div class="tab_content" tab="pagination">
-				<h4>Pagination</h4>
-				<div class="pagination row">
-					<a href="#0" class="prev">Prev</a>
-					<a href="#0">1</a>
-					<a href="#0" class="active">2</a>
-					<a href="#0">3</a>
-					<a href="#0">4</a>
-					<a href="#0" class="next">Next</a>
-				</div>
+			<div class="tab_content" tab="tab2">
+				<h4>Tab 2</h4>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sagittis urna mi, nec scelerisque lacus tempor in. Sed congue lacus quis diam condimentum malesuada. Duis finibus volutpat neque, a tincidunt dui sagittis eget. Aenean in purus dignissim dui gravida vulputate. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesent consectetur sit amet arcu vitae efficitur. Nullam varius mi at tellus scelerisque efficitur. Nam in dui dui. Cras maximus urna vitae dolor auctor tincidunt. Aliquam in semper sem. Pellentesque dignissim venenatis ullamcorper. Integer ac dolor consectetur, aliquet nulla a, rhoncus arcu.</p>
+				<p>Fusce quis posuere est, sed tempus nisl. Duis finibus ullamcorper aliquam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Maecenas imperdiet lobortis magna vel volutpat. Cras augue dui, tempor eget eleifend eget, tempor vitae enim. Mauris nulla sapien, viverra quis nisi eget, tempus ultricies libero. Donec non tristique nisi, sed accumsan mi. Etiam quis nisi metus. Proin volutpat eu odio maximus efficitur. Quisque sagittis rutrum velit, sit amet accumsan quam consectetur ac. Donec non nisl vel tellus lacinia pellentesque vel ac sapien. Nam iaculis porta posuere. i</p>
 			</div>
 
-			<div class="tab_content" tab="tooltips">
-				<h4>Tooltips</h4>
-				
+			<div class="tab_content" tab="tab3">
+				<h4>Tab 3</h4>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sagittis urna mi, nec scelerisque lacus tempor in. Sed congue lacus quis diam condimentum malesuada. Duis finibus volutpat neque, a tincidunt dui sagittis eget. Aenean in purus dignissim dui gravida vulputate. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesent consectetur sit amet arcu vitae efficitur. Nullam varius mi at tellus scelerisque efficitur. Nam in dui dui. Cras maximus urna vitae dolor auctor tincidunt. Aliquam in semper sem. Pellentesque dignissim venenatis ullamcorper. Integer ac dolor consectetur, aliquet nulla a, rhoncus arcu.</p>
+				<p>Fusce quis posuere est, sed tempus nisl. Duis finibus ullamcorper aliquam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Maecenas imperdiet lobortis magna vel volutpat. Cras augue dui, tempor eget eleifend eget, tempor vitae enim. Mauris nulla sapien, viverra quis nisi eget, tempus ultricies libero. Donec non tristique nisi, sed accumsan mi. Etiam quis nisi metus. Proin volutpat eu odio maximus efficitur. Quisque sagittis rutrum velit, sit amet accumsan quam consectetur ac. Donec non nisl vel tellus lacinia pellentesque vel ac sapien. Nam iaculis porta posuere. i</p>
 			</div>
+		</div>
 
-			<div class="tab_content" tab="modal">
-				<h4>Modal</h4>
-				
-			</div>
+		<h4>Pagination</h4>
+		<div class="pagination row">
+			<a href="#0" class="prev">Prev</a>
+			<a href="#0">1</a>
+			<a href="#0" class="active">2</a>
+			<a href="#0">3</a>
+			<a href="#0">4</a>
+			<a href="#0" class="next">Next</a>
 		</div>
 
 		<h3>Messages</h3>
