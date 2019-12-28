bc4db91c456ede561657777e4bfffab3208f0891
Glade Lynch <blooblahguy@gmail.com>
2019-12-27 21:52:13 -0700

    changing compile method

---
+++ style.php
@@ -0,0 +1,20 @@
+<?php
+	header('Content-Type: text/css');
+
+	$out_file = "openskull.min.css";
+
+	$sheets = array();
+	$sheets[] = "openskull/_functions.scss";
+	$sheets[] = "_variables.scss";
+	$sheets[] = "openskull/_reset.scss";
+	$sheets[] = "openskull/_colors.scss";
+	$sheets[] = "openskull/_buttons.scss";
+	$sheets[] = "openskull/_typography.scss";
+	$sheets[] = "openskull/_helpers.scss";
+	$sheets[] = "openskull/_forms.scss";
+	$sheets[] = "openskull/_ui.scss";
+	$sheets[] = "openskull/_grid.scss";
+	$sheets[] = "style.scss";
+
+	require_once("_style_compile.php");
+?>
