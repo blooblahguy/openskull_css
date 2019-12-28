50e35b91360d423d7c71a20f7528c607ddb3a643
Glade Lynch <blooblahguy@gmail.com>
2019-12-27 20:44:47 -0700

    recent demo updates

--- demo/footer.php
+++ demo/footer.php
@@ -1,57 +1,113 @@
 		
+
 		</div> <!-- .main_content -->
+
 		<div class="footer_outer pad1 bg-black">
+
 			<div class="footer container em muted text-right">
+
 				&copy; <? echo Date("Y"); ?> all rights reserverd &bull; <a href="http://www.gladelynch.com" target="_blank">Glade Lynch</a>
+
 			</div>
+
 		</div>
+
 	</div> <!-- .wrapper -->
 
+
+
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
 	<script>
+
 		// this trims extra whitespace and stuff inside of code prettify
+
 		$("code").each(function() { 
+
 			var target = $(this).attr("target")
 
+
+
 			var html
+
 			if (target) {
+
 				html = $(target).html()
+
 			} else {
+
 				html = $(this).html()
+
 			}
+
+
 
 			// undent code to base line
+
 			var lines = html.split('\n');
+
 			var tabs
+
 			$.each(lines, function() {
+
 				tabs = (this.match(/\t/g) || []).length;
+
 				if (tabs > 0) {
+
 					return;
+
 				}
+
 			})
+
 			tabs += 1
+
 			var exp = '\n';
+
 			var test = '';
+
 			for ( i = 1; i <= tabs; i++) {
+
 				exp += '\t'
+
 				test += 't';
+
 			}
+
 			exp = new RegExp(exp, "gi")
-			console.log(target, lines.length, tabs, test)
+
+			//console.log(target, lines.length, tabs, test)
+
 			
+
 		
+
 			html = html.replace(exp, "\n").trim()
 
+
+
 			if (/<[a-z/][\s\S]*>/i.test(html)) {
+
 				$(this).text(html) // .text auto trims
+
 			} else {
+
 				$(this).html(html) // .text auto trims
+
 			}
+
 		}); 
+
 	</script>
 
+
+
 	<script src="/openskull.js"></script>
+
 	<script src="/demo/prettify.js"></script>
+
 	<script> hljs.initHighlightingOnLoad();</script>
+
 </body>
+
 </html>
--- demo/header.php
+++ demo/header.php
@@ -2,6 +2,16 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-136145917-1"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+		gtag('config', 'UA-136145917-1');
+	</script>
+
 	<title>Openskull framework</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -10,27 +20,35 @@
 </head>
 <body id="body">
 	<div class="wrapper">
-		<div class="header_outer pad2 lg-pad0 text-right lg-text-left bg-black fixed t l r">
-			<ul class="jump_nav bg-black container os_menu">
-				<li><a href="#intro" class="text-primary">Openskull</a></li>
-				<li><a href="#grid">Grid</a></li>
-				<li><a href="#layout">Layout</a></li>
-				<li><a href="#type">Typography</a></li>
-				<li><a href="#forms">Forms</a></li>
-				<li><a href="#tables">Tables</a></li>
-				<li><a href="#colors">Colors</a></li>
-				<li><a href="#elements">Elements</a></li>
-			</ul>
-			<? // doing both of these gives us javascript-less hamburger menus ?>
-			<a href="#0" class="os_menu_button close">
-				<span></span>
-				<span></span>
-			</a>
-			<a href="#body" class="os_menu_button open">
-				<span></span>
-				<span></span>
-				<span></span>
-			</a>
+		<div class="header_outer pad2 lg-pad0 text-right lg-text-left bg-black fixed t w100">
+			<div class="container">
+				<div class="row content-center">
+					<div class="os-min pad1">
+						<a href="#intro" class="text-primary strong">Openskull</a>
+					</div>
+					<ul class="jump_nav bg-black os_menu os-min mobile_menu">
+						<li><a href="#grid">Grid</a></li>
+						<li><a href="#layout">Layout</a></li>
+						<li><a href="#type">Typography</a></li>
+						<li><a href="#forms">Forms</a></li>
+						<li><a href="#tables">Tables</a></li>
+						<li><a href="#colors">Colors</a></li>
+						<li><a href="#elements">Elements</a></li>
+					</ul>
+					<? // doing both of these gives us javascript-less hamburger menus ?>
+					<a href="#0" class="os_menu_button close">
+						<span></span>
+						<span></span>
+					</a>
+					<a href="#body" class="os_menu_button open">
+						<span></span>
+						<span></span>
+						<span></span>
+					</a>
+				</div>
+			</div>
+			
+			
 		</div>
 		
 		<div class="main_content" style="padding-top: 48px">
